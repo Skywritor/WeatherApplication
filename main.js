10d883cd3281b520fd30c40cb556c3d468f3728b
l4nk332 <l4nk332@gmail.com>
2016-03-02 14:21:51 -0800

    Updates 'index.html' to include a text box for collection of user input. Adds functionality to 'main.js', making an AJAX request to openweathermap API when user clicks, handling parsing of JSON data, and properly displaying response to client. Adds new API parameter information in 'openweathermap_api.md.'

--- main.js
+++ main.js
@@ -1,17 +1,29 @@
 var trigger = document.getElementById('trigger');
 
 trigger.addEventListener('click', function() {
+    // Grab Zip Code value from input box
+    var zipCode = document.getElementById('zipcode').value;
+    // Set up api url
+    var url = 'http://api.openweathermap.org/data/2.5/weather';
+    var apiKey = '498a1c465e5ecc15a5e913beb7ef67b3';
+    var query = url + '?zip=' + zipCode + 'us&appid=' + apiKey;
+
+    // Create XHR Instance
     var request = new XMLHttpRequest();
-
-    request.open('GET', 'weather_info.json');  
-
+    // Request Weather data api
+    request.open('GET', query);
+    // Send request
     request.send();
-
+    // When readyState changes
     request.onreadystatechange = function() {
+        // If status OK and request is ready (4)
         if(request.status === 200 && request.readyState === 4){
+            // Grab weather information
             var currentWeather = document.getElementById('current-weather');
+            // Parse response from api
             var weatherData = JSON.parse(request.response);
-            currentWeather.innerHTML = weatherData.weather[0].description
+            // Set currentWeather to display weather data
+            currentWeather.innerHTML = weatherData.weather[0].description;
         }
-    }
+    };
 });
